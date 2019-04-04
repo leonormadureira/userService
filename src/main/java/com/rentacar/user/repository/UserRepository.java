224ae6eb9793eb7e2a22604ee5cb1fd6384e254c
Leonor Madureira <NB20985@novabase.pt>
2019-04-04 09:33:54 +0100

    .

--- src/main/java/com/rentacar/user/repository/UserRepository.java
+++ src/main/java/com/rentacar/user/repository/UserRepository.java
@@ -8,9 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository <User, Long> {
 
-    @Query(value = "SELECT * FROM User WHERE user_id = ?1", nativeQuery = true)
-    User findUserByUserId(Long user_id);
-
     @Query(value = "SELECT * FROM User WHERE nif = ?1", nativeQuery = true)
     User findUserByNif(Long nif);
 }
